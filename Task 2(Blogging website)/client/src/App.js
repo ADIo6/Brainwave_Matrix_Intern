a94ae544997b8c1ad0fbd6958bec609ebda680b2
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-11-29 16:49:35 +0530

    Created Login and SignUp page(frontend)

---
+++ Task 2(Blogging website)/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// components
+import Login from './component/account/Login'
+
+function App() {
+  return (
+    <div style = {{marginTop: 60}}>
+        <Login />
+    </div>
+  );
+}
+
+export default App;
